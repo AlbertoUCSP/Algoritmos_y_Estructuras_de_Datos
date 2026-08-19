582f3843ca040501df04ec264e2d75ee7322e2b9
LescanoDUX <lescano.dux@gmail.com>
2026-08-18 22:11:33 -0500

    punteros dobles y acceso al contenido

--- Temas/00_repaso/02_ptrs_dobles_triples.cpp
+++ Temas/00_repaso/02_ptrs_dobles_triples.cpp
@@ -3,8 +3,12 @@ using namespace std;
 
 int main() {
 
-    
+    int a = 10;
+    int* p = &a;
+    cout << "direccion de a: " << p << "\ncontenido de a: " << *p << endl; 
 
+    int** q = &p;
+    cout << "direccion de p: " << q << "\ncontenido de p: " << *q << endl;  
 
 
     return 0;
