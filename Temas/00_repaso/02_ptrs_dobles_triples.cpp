2a208659573b24c379aa912e8e3de91d272fd485
LescanoDUX <lescano.dux@gmail.com>
2026-08-19 19:22:11 -0500

    contenido del contenido de punteros

--- Temas/00_repaso/02_ptrs_dobles_triples.cpp
+++ Temas/00_repaso/02_ptrs_dobles_triples.cpp
@@ -8,8 +8,20 @@ int main() {
     cout << "direccion de a: " << p << "\ncontenido de a: " << *p << endl; 
 
     int** q = &p;
-    cout << "direccion de p: " << q << "\ncontenido de p: " << *q << endl;  
+    cout << "direccion de p: " << q << "\ncontenido de p: " << **q << endl;  
 
+    int*** z = &q;
+    cout << "direccion de q: " << z << "\ncontenido de q: " << **z << endl;
+
+    // Modificando a desde z
+    cout << "modificando a..." << endl;
+    ***z = 1000;
+    cout << "nuevo contenido de a: " << a << endl; // a = ***z
+
+    cout << "contenido actual de q: " << **z << "\ncambiando contenido de q..." << endl; 
+    int b = 500;
+    **z = &b;
+    cout << "nuevo contenido de q: " << **z << endl;
 
     return 0;
 }
