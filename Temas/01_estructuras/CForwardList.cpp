a301dd4065cd1a4c63a8d8ca1ed4a5306d0747b2
LescanoDUX <lescano.dux@gmail.com>
2026-09-01 15:06:01 -0500

    CForwardList implementación de métodos principales

--- Temas/01_estructuras/CForwardList.cpp
+++ Temas/01_estructuras/CForwardList.cpp
@@ -7,6 +7,7 @@ struct Node {
 
     Node(int n) {
         value = n;
+        next = nullptr;
     }
 };
 
@@ -18,41 +19,60 @@ public:
     CForwarList(int n) {
         Node* firstNode = new Node(n); // creamos el primer nodo con el valor n
         head = firstNode;              // head apunta al primer nodo
-        firstNode->next = nullptr;     
     }
 
-    ~CForwarList() {
-
+    ~CForwarList() { /**ANALIZAR**/
+        Node* tmp;
+        for (tmp = head; head != nullptr; head = tmp) {
+            tmp = head->next;
+            delete head;
+        }
+        delete head;
     }
 
     void push_back(int n) {
+        Node* newNode = new Node(n); // creamos un nuevo nodo
+        Node* p = head;              // p nos ayudará a recorrer la lista
+        while (p->next != nullptr) { 
+            p = p->next;             // vamos actualizando p 
+        }
+        p->next = newNode;           // conectamos el nuevo nodo con la lista
+    }
+
+    void push_front(int n) {
         Node* newNode = new Node(n);
+        newNode->next = head;
+        head = newNode;
+    }
+
+    void pop_back() {
+        Node* prev;      // puntero al nodo anterior
+        Node* p = head;
+        while (p->next != nullptr) {
+            prev = p;    // prev irá avanzando a la posición de p
+            p = p->next; // luego avanzamos p
+            
+        }
+        prev->next = p->next; // el penultimo nodo(que ahora será el último) apunta a nullptr
+        delete p;             // liberamos la memoria del ultimo nodo usando p
+    }
+
+    void pop_front() { //**ANALIZAR**/
+        Node* tmp = head->next; // puntero temporal para la eliminación del nodo
+        delete head;
+        head = tmp;
+    }
+
+    int front() {
+        return head->value;
+    }
+
+    int back() {
         Node* p = head;
         while (p->next != nullptr) {
             p = p->next;
         }
-        p->next = newNode;
-        newNode->next = nullptr;
-    }
-
-    void push_front(int n) {
-
-    }
-
-    void pop_back() {
-
-    }
-
-    void pop_front() {
-
-    }
-
-    int front() {
-
-    }
-
-    int back() {
-        
+        return p->value;
     }
 
     void print() {
@@ -60,7 +80,7 @@ public:
         for (p = head; p != nullptr; p = p->next) {
             cout << p->value << "->";
         }
-        cout << "\n";
+        cout << "NULL\n";
     }
 };
 
@@ -69,4 +89,20 @@ int main() {
     fl.print();
     fl.push_back(200);
     fl.print();
+    fl.push_back(300);
+    fl.print();
+    fl.push_front(50);
+    fl.print();
+    fl.pop_front();
+    fl.print();
+    cout << "Front: " << fl.front() << endl;
+    cout << "Back: " << fl.back() << endl;
+    fl.push_back(400);
+    fl.print();
+    fl.push_front(25);
+    fl.print();
+    fl.pop_front();
+    fl.print();
+
+    return 0;
 }
